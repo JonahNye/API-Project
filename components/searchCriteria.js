0dce684a6e3ae2a9e006d1d23c58a9b313ca308d
Jonah Nye <Jonahnye94@gmail>
2018-11-13 15:25:04 -0500

    search page intial styling

--- components/searchCriteria.js
+++ components/searchCriteria.js
@@ -2,28 +2,35 @@
 
 const searchCriteria = {
   template: `
-  <button ng-click="$ctrl.goFavorites();">Favorites</button>
-  <input type="text" placeholder="recipe" ng-model="$ctrl.recipe">
-  <label>By Molecule</label>
-  <select ng-model="$ctrl.healthy">
-    <option>low-carb</option>
-    <option>low-fat</option>
-    <option>high-protein</option>
-  </select>
-  <label>Dietary Restrictions</label>
-  <select ng-model="$ctrl.restrictions">
-    <option>peanut-free</option>
-    <option>alcohol-free</option>
-    <option>vegan</option>
-    <option>vegetarian</option>
-  </select>
-  <button ng-click="$ctrl.search($ctrl.recipe, $ctrl.healthy, $ctrl.restrictions);">Search A Recipe</button>
-  <recipe-list list-of-ingredients="$ctrl.listOfIngredients" list-of-items="$ctrl.listOfItems"></recipe-list>
-  
+  <i class="material-icons favorites-page-btn" ng-click="$ctrl.goFavorites();">favorite</i>
+  <header>
+    <h1>Recipease</h1>
+    <p class="sub-title">Search Recipes With Ease</p>
+  </header>  
+ <section class="search-center-container">
+  <section class="search-container">
+    <label>By Molecule</label>
+    <select ng-model="$ctrl.healthy">
+      <option>low-carb</option>
+      <option>low-fat</option>
+      <option>high-protein</option>
+    </select>
+    <label>Dietary Restrictions</label>
+    <select ng-model="$ctrl.restrictions">
+      <option>peanut-free</option>
+      <option>alcohol-free</option>
+      <option>vegan</option>
+      <option>vegetarian</option>
+    </select>
+    <input class="search-bar" type="text" placeholder="recipe" ng-model="$ctrl.recipe">
+    <img class="carrot-arrow" src="./images/carrot-arrow.png" ng-click="$ctrl.search($ctrl.recipe, $ctrl.healthy, $ctrl.restrictions);"/>
+    <recipe-list list-of-ingredients="$ctrl.listOfIngredients" list-of-items="$ctrl.listOfItems"></recipe-list>
+   </section> 
+ </section>
 `,
   controller: ["Service", "$location", function (Service, $location) {
     const vm = this;
-            //concatenate?
+
     vm.search = (recipe, healthy, restrictions) => {
       Service.searchRecipe(recipe, healthy, restrictions).then((response) => {
         console.log(response);
