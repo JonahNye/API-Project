785429298e24e1edc736f1b6490ec0b21ffef894
Claboom86 <betsybass@gmail.com>
2018-11-13 15:25:13 -0500

    Began styling

--- components/favoritesPage.js
+++ components/favoritesPage.js
@@ -2,12 +2,16 @@
 
 const favoritesPage = {
   template: `
-    <section ng-repeat="favorite in $ctrl.favoritesList track by $index">
-        <p> Diet: {{ favorite.recipe.dietLabels[0] }} </p>
-        <p> Servings: {{ favorite.recipe.yield }} </p>
-        <p> Link to recipe: {{ favorite.recipe.url }} </p>
-        <i class="fas fa-minus" ng-click="$ctrl.remove($index)"></i>
+  <h3>Favorites</h3>
+  <section id="main-favorite">
+    <section id="favorite" ng-repeat="favorite in $ctrl.favoritesList track by $index">
+        <i id="delete-btn" class="fa fa-times" ng-click="$ctrl.remove($index)"></i>
+        <p class="favorite-info"> Diet: {{ favorite.recipe.dietLabels[0] }} </p>
+        <p class="favorite-info"> Servings: {{ favorite.recipe.yield }} </p>
+        <p class="favorite-info"> Link to recipe: {{ favorite.recipe.url }} </p>
+        <img id="favorite-image" src="{{ favorite.recipe.image }}">
     </section>
+  </section>
   `,
   controller: ["Service", function (Service) {
     const vm = this;
