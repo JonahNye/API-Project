afd0fbfd8e2cc0e136509b32fef8812944a302d3
Claboom86 <betsybass@gmail.com>
2018-11-13 13:21:14 -0500

    Added delete button

--- components/favoritesPage.js
+++ components/favoritesPage.js
@@ -2,15 +2,21 @@
 
 const favoritesPage = {
   template: `
-    <section ng-repeat="favorite in $ctrl.favoritesList">
+    <section ng-repeat="favorite in $ctrl.favoritesList track by $index">
         <p> Diet: {{ favorite.recipe.dietLabels[0] }} </p>
-        <p> Total time: {{ favorite.recipe.totalTime }} </p>
+        <p> Servings: {{ favorite.recipe.yield }} </p>
         <p> Link to recipe: {{ favorite.recipe.url }} </p>
+        <i class="fas fa-minus" ng-click="$ctrl.remove($index)"></i>
     </section>
   `,
   controller: ["Service", function (Service) {
     const vm = this;
     vm.favoritesList = Service.getFavorites();
+    vm.remove = (index) => {
+      Service.removeFavorite(index);
+    };
+
+
   }]
 }
 
